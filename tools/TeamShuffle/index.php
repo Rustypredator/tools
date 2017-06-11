7e23d995c06702442a48f34b44c90300913aaa2b
Florian Steltenkamp <florian@steltenkamp.de>
2017-06-11 15:09:09 +0200

    asfs

--- tools/TeamShuffle/index.php
+++ tools/TeamShuffle/index.php
@@ -26,10 +26,9 @@
         <title>TeamShuffle - Rusty's Tools</title>
         <meta encoding="UTF-8"/>
         <meta title="StrRev - Rusty's Tools" description=""/>
-        <link rel="stylesheet" href="style/css/bootstrap.min.css" />
-        <script src="../style/js/jquery-3.2.1.min.js"></script>
-        <script src="../style/js/bootstrap.min.js"></script>
-        <script src="../style/js/main.js"></script>
+        <link rel="stylesheet" href="https://tools.rusty.info/style/css/bootstrap.min.css" />
+        <script src="https://tools.rusty.info/style/js/jquery-3.2.1.min.js"></script>
+        <script src="https://tools.rusty.info/style/js/bootstrap.min.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-default">
@@ -57,7 +56,7 @@
                 <div class="panel panel-success">
                     <div class="panel-heading" href="#info-collapse" data-target="#info-collapse" data-toggle="collapse">Intro (click me for info)</div>
                     <div class="panel-body collapse" id="info-collapse">
-                        <h3>TeamShuffle</h3><small>String Reverse</small>
+                        <h3>TeamShuffle</h3>
                         <p>
                             This Tools aims at Groups who have trouble forming Teams.<br/>
                             You can put in any number of Names and The Tool will form a Team for you.
@@ -71,13 +70,9 @@
                     <div class="panel-body">
                         <form action="" method="POST">
                             <label for="players">Comma Separated List of your Players:</label>
-                            <textarea name="players" class="form-control">
-                                This list has to be Comma Separated like this:<br/>
-                                john doe,jane doe,max,peter,mali<br/>
-                                (You'll have to delete this text ;) )
-                            </textarea>
+                            <textarea name="players" class="form-control" rows="15"><?php if ($_POST and isset($_POST['players'])) echo $_POST['players']; else echo "This list has to be Comma Separated like this:\njohn doe,jane doe,max,peter,mali\n(You'll have to delete this text ;) )";?></textarea>
                             <label for="teams">Amount of Teams</label>
-                            <input type="number" name="teams" value="2"/>
+                            <input type="number" name="teams" class="form-control" <?php if ($_POST and isset($_POST['teams'])) echo 'value="'.$_POST['teams'].'"'; else echo 'value="2"';?>/>
                             <input type="submit" name="submit" class="btn btn-success form-control"/>
                         </form>
                     </div>
@@ -90,12 +85,33 @@
                     <table class="table table-striped table-hover table-condensed">
                         <?php
                             $data = $_POST;
-                            $teams = $data['teams'];
-                            $players = $data['players'];
-                            echo "teams:";
-                            var_dump($teams);
-                            echo "<br/>players:";
+                            $teamscount = $data['teams'];
+                            $players = explode(",", $data['players']);
+                            $playercount = count($players);
+                            if ($teamscount < 2) {
+                                echo "<div class=\"alert alert-warning\">Why would you want to shuffle the players in one team?</div>";
+                            }
+                            echo "teams: ";
+                            var_dump($teamscount);
+                            echo "<br/>players: ";
                             var_dump($players);
+                            echo "<br/>Playercount: ";
+                            var_dump($playercount);
+                            $teams = array();
+                            for ($i = 1; $i <= $teamscount; $i++) {
+                                $teams[$i] = array();
+                            }
+                            echo "<br/>Teams: ";
+                            var_dump($teams);
+                            while(count($players)) {
+                                for ($i = 1; $i < count($teams); $i++) {
+                                    $rand = rand(0,count($players));
+                                    $teams[$i][] = $players[$rand];
+                                    unset($players[$rand]);
+                                }
+                            }
+                            echo "<br/>Teams: ";
+                            var_dump($teams);
                         ?>
                     </table>
                     <div class="panel-footer"></div>
