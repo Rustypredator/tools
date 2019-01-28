fc56d2ce5ebbef247604392bb75099fbe2e33717
Rustypredator <contact@rusty.info>
2019-01-28 19:39:46 +0100

    first test of nameresolver

--- tools/secompcalc/nameresolv.php
+++ tools/secompcalc/nameresolv.php
@@ -1,3 +1,23 @@
 <?php
-    echo "lol";
-?>
+    $partlist_object = json_decode(file_get_contents("parts.json"));
+
+    $data = $_POST;
+    if (!$data) {
+        echo json_encode(array("success"=>false, "message"=>"There was no Data submitted!"));
+    }
+
+    if (!isset($data["ident"]) || empty($data["ident"]) || $data["ident"] === "") {
+        echo json_encode(array("success"=>false, "message"=>"There was no ident submitted!"));
+    }
+
+    $label = "";
+    $ident = $data["ident"];
+    $blocks = $partlist_object->blocks;
+    foreach ($blocks as $block) {
+        if ($block->ident === $ident) {
+            $name = $block->label;
+            echo json_encode(array("success"=>false, "message"=>"Label for $ident was found.", "name"=>$name));
+            return;
+        }
+    }
+    echo json_encode(array("success"=>false, "message"=>"We could not find the label for $ident."));
