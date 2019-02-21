d6a6ebdf96f2be29e4cd1dc6633d4c0c04d9e0ca
Florian <florian@steltenkamp.de>
2019-02-21 15:58:31 +0100

    updated secompcalc

--- tools/secompcalc/index.php
+++ tools/secompcalc/index.php
@@ -20,105 +20,104 @@
  *
  * @author Florian Steltenkamp <contact@rusty.info>
  */
-require_once("../functions.php");
-
+require "../../init.php";
+$toolname = "secompcalc";
 $partlist_object = json_decode(file_get_contents("parts.json"));
 ?>
 <html>
     <head>
-        <title>SECompCalc - Rusty's Tools</title>
-        <link rel="stylesheet" href="../../style/css/main.css"/>
+        <title>Rusty's Tools - SeCompCalc</title>
+        <?php include $templatedir."head/cssfiles.phtml"; ?>
     </head>
     <body>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="https://tools.rusty.info">Rusty's Tools</a>
-                </div>
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="https://tools.rusty.info/tools/secompcalc">SECompCalc <span class="sr-only">(current)</span></a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div class="container container-fluid">
-            <div class="col-md-12">
-                <?php
-                    $content = "<h3>SpaceEngineers Component Calculator</h3><p>This tool allows you to calculate all the components and raw resources you will need for your build.<br/>Just add the blocks you need and watch as the tool calculates what components you will need.<br/>Feedback welcome!</p>";
-                    printBootstrapPanel("secompcalc_info", "success", "Info (click me)", $content, true, false);
-                ?>
-            </div>
-            <div class="col-md-12">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">Add Blocks</div>
-                    <div class="panel-body">
-                        <p>
-                            Select a Part from the List below and click the "Add" Button to add it to the list.
-                        </p>
-                        <div class="col-sm-6">
-                            <select id="partpicker-part" name="part" class="part-selector form-control">
-                                <option value="null">-- Select Part to add --</option>
-                                <?php foreach ($partlist_object->blocks as $block): ?>
-                                <?php
-                                    $ident = $block->ident;
-                                    $label = $block->label;
-                                    $ingredients = $block->ingredients;
-                                ?>
-                                <option value="<?php echo $ident; ?>"><?php echo $label; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                        </div>
-                        <div class="col-sm-2">
-                            <select id="partpicker-gridsize" name="gridsize" class="gridsize-selector form-control">
-                                <option value="large">Large</option>
-                                <option value="small">Small</option>
-                            </select>
-                        </div>
-                        <div class="col-sm-2">
-                            <input id="partpicker-amount" type="number" value="1" min="1" max="1000" class="form-control"/>
-                        </div>
-                        <div class="col-sm-2">
-                            <button class="btn btn-success"><i class="fa fa-lg fa-plus" onclick="addpart()"></i>&nbsp;Add</button>
+        <?php
+            $additionalNavItems = "<li class='nav-item'><a class='nav-link' href='".$baseurl."'>Home</a></li>";
+            $additionalNavItems .= "<li class='nav-item active'><a class='nav-link' href='".$baseurl.$toolname."'>SeCompCalc <span class='sr-only'>(current)</span></a></li>"
+        ?>
+        <?php $additionalNavItemsRight = ""; ?>
+        <?php include $templatedir."navbar.phtml"; ?>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3 col-sm-12">
+                    <?php include $templatedir."toolnav.phtml"; ?>
+                    <div class="card text-white bg-success" style="margin-top:15px;">
+                        <div class="card-header collapsed" type="button" data-toggle="collapse" data-target="#infoCollapse" aria-expanded="false" aria-controls="infoCollapse">INFO ( Click me! )</div>
+                        <div class="card-body collapse" id="infoCollapse">
+                            This Tool is currently WIP!
+                            <h3>SpaceEngineers Component Calculator</h3>
+                            <p>
+                                This tool allows you to calculate all the components and raw resources you will need for your build.<br/>
+                                Just add the blocks you need and watch as the tool calculates what components you will need.<br/>
+                                Feedback welcome!
+                            </p>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6 col-sm-12">
-                <div class="panel panel-info">
-                    <div class="panel-heading">Added Blocks (newest on top)</div>
-                    <table class="panel-body table table-striped table-condensed table-hover" id="partlist">
-                        <thead>
-                            <th>Name</th>
-                            <th>Amount</th>
-                            <th>Size</th>
-                            <th>Actions</th>
-                        </thead>
-                        <tbody id="partlist_tbody">
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div class="col-md-6 col-sm-12">
-                <div class="panel panel-success">
-                    <div class="panel-heading">Parts needed:<button class="pull-right btn btn-success btn-sm"><i class="fa fa-lg fa-refresh"></i>&nbsp;Recalculate</button></div>
-                    <div class="panel-body">
-                        -
+                <div class="col-md-9 col-sm-12">
+                    <div class="row" style="margin-bottom:15px;">
+                        <div class="col-sm-12 col-md-6">
+                            <div class="card text-white bg-dark">
+                                <div class="card-header">Add Blocks</div>
+                                <div class="card-body">
+                                    <p>
+                                        Select a Part from the List below and click the "Add" Button to add it to the list.
+                                    </p>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <select id="partpicker-part" name="part" class="part-selector form-control">
+                                                <option value="null">-- Select Part to add --</option>
+                                                <?php foreach ($partlist_object->blocks as $block): ?>
+                                                    <?php
+                                                    $ident = $block->ident;
+                                                    $label = $block->label;
+                                                    $ingredients = $block->ingredients;
+                                                    ?>
+                                                    <option value="<?php echo $ident; ?>"><?php echo $label; ?></option>
+                                                <?php endforeach; ?>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <select id="partpicker-gridsize" name="gridsize" class="gridsize-selector form-control">
+                                                <option value="large">Large</option>
+                                                <option value="small">Small</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <input id="partpicker-amount" type="number" value="1" min="1" max="1000" class="form-control"/>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <button class="btn btn-success"><i class="fa fa-lg fa-plus" onclick="addpart()"></i>&nbsp;Add</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-6">
+                            <div class="card text-white bg-dark">
+                                <div class="card-header">Added Blocks (newest on top)</div>
+                                <table class="card-body table table-striped table-condensed table-hover text-light" id="partlist">
+                                    <thead>
+                                        <th>Name</th>
+                                        <th>Amount</th>
+                                        <th>Size</th>
+                                        <th>Actions</th>
+                                    </thead>
+                                    <tbody id="partlist_tbody">
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card text-light bg-secondary">
+                        <div class="card-header"><button class="btn btn-primary"><i class="fa fa-sync"></i></button>&nbsp;&nbsp;Parts needed:</div>
+                        <div class="card-body">
+                            {content}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <script src="../../style/js/jquery-3.2.1.min.js"></script>
-        <script src="../../style/js/bootstrap.min.js"></script>
-        <script src="../../style/js/select2.min.js"></script>
-        <script src="../../style/js/toastr.min.js"></script>
-        <script src="../../style/js/main.js"></script>
+        <?php include $templatedir."head/jsfiles.phtml"; ?>
         <script>
             $(document).ready(function() {
                 $('.part-selector').select2();
@@ -187,6 +186,4 @@ $partlist_object = json_decode(file_get_contents("parts.json"));
             }
         </script>
     </body>
-    <footer>
-    </footer>
 </html>
