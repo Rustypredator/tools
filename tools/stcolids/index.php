38f0fec08f39d4f26999ff4ec018654941fb1fd7
Florian <florian@steltenkamp.de>
2019-02-21 16:23:01 +0100

    updated stcolids for use with bs4

--- tools/stcolids/index.php
+++ tools/stcolids/index.php
@@ -1,121 +1,145 @@
 <?php
-    /**
-     * stcolids
-     * Copyright (C) 2017  rusty.info
-     *
-     * Git: https://gitlab.com/rustyinfo/tools-homepage
-     *
-     * This program is free software: you can redistribute it and/or modify
-     * it under the terms of the GNU General Public License as published by
-     * the Free Software Foundation, either version 3 of the License, or
-     * (at your option) any later version.
-     *
-     * This program is distributed in the hope that it will be useful,
-     * but WITHOUT ANY WARRANTY; without even the implied warranty of
-     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     * GNU General Public License for more details.
-     *
-     * You should have received a copy of the GNU General Public License
-     * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-     *
-     * @author Florian Steltenkamp <contact@rusty.info>
-     */
+/**
+ * stcolids
+ * Copyright (C) 2017  rusty.info
+ *
+ * Git: https://gitlab.com/rustyinfo/tools-homepage
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Florian Steltenkamp <contact@rusty.info>
+ */
+require "../../init.php";
+$toolname = "StColIDs";
+$toolshort = strtolower($toolname);
+$toolDescShort = "";
 ?>
 <html>
-    <head>
-        <title>StColIDs - Rusty's Tools</title>
-        <meta encoding="UTF-8"/>
-        <meta title="StColIDs - Rusty's Tools" description="Tool to extract Mod-IDs from Steam collections"/>
-        <link rel="stylesheet" href="../../style/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="../../style/css/font-awesome.min.css" />
-        <script src="../../style/js/jquery-3.2.1.min.js"></script>
-        <script src="../../style/js/bootstrap.min.js"></script>
-        <script src="main.js"></script>
-    </head>
+    <?php include $templatedir."head/head.phtml"; ?>
     <body>
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="https://tools.rusty.info">Rusty's Tools</a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="https://tools.rusty.info/tools/stcolids">StColIDs <span class="sr-only">(current)</span></a></li>
-                    </ul>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
-        <div class="container container-fluid">
-            <div class="col-md-12">
-                <div class="panel panel-success">
-                    <div class="panel-heading" href="#info-collapse" data-target="#info-collapse" data-toggle="collapse">Intro (click me for info)</div>
-                    <div class="panel-body collapse" id="info-collapse">
-                        <h3>StColIDs</h3><small>Steam Collection IDs</small>
-                        <p>
-                            This tool allows you, to extract all Workshop mod IDs from a Collection.<br/>
-                            This is <b>very</b> usefull for Games like Space Engineers where you have to put in a list of mods into the server config<br/>
-                            ( at least for the dedicated servers )<br/>
-                            Feel free to use it for whatever reason, Spaceengineers was why i buildt this tool.<br/>
-                        </p><br/>
-                        <h4>Guide:</h4>
-                        <p>
-                            It is very simple to use this Tool.<br/>
-                            <ul>Steps:
-                                <li>1. Get the Link to your Collection</li>
-                                <li>2. grab <b>only</b> the ID at the end.</li>
-                                <li>3. Put this id into the "ID" field</li>
-                                <li>4. Push le button</li>
-                            </ul>
-                            Thats it. not that hard, right?<br/>
-                            I hope this tool will help you as much as it has helped me ;)
-                        </p>
+        <?php
+        $additionalNavItems = "<li class='nav-item'><a class='nav-link' href='".$baseurl."'>Home</a></li>";
+        $additionalNavItems .= "<li class='nav-item active'><a class='nav-link' href='".$baseurl.$toolname."'>StColIDs <span class='sr-only'>(current)</span></a></li>"
+        ?>
+        <?php $additionalNavItemsRight = ""; ?>
+        <?php include $templatedir."navbar.phtml"; ?>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3 col-sm-12">
+                    <?php include $templatedir."toolnav.phtml"; ?>
+                    <div class="card text-white bg-success" style="margin-top:15px;">
+                        <div class="card-header collapsed" type="button" data-toggle="collapse" data-target="#infoCollapse" aria-expanded="false" aria-controls="infoCollapse">INFO ( Click me! )</div>
+                        <div class="card-body collapse" id="infoCollapse">
+                            <h3>StColIDs</h3><small>Steam Collection IDs</small>
+                            <p>
+                                This tool allows you, to extract all Workshop mod IDs from a Collection.<br/>
+                                This is <b>very</b> usefull for Games like Space Engineers where you have to put in a list of mods into the server config<br/>
+                                ( at least for the dedicated servers )<br/>
+                                Feel free to use it for whatever reason, Spaceengineers was why i buildt this tool.<br/>
+                            </p><br/>
+                            <h4>Guide:</h4>
+                            <p>
+                                It is very simple to use this Tool.<br/>
+                                <ul>Steps:
+                                    <li>1. Get the Link to your Collection</li>
+                                    <li>2. grab <b>only</b> the ID at the end.</li>
+                                    <li>3. Put this id into the "ID" field</li>
+                                    <li>4. Push le button</li>
+                                </ul>
+                                Thats it. not that hard, right?<br/>
+                                I hope this tool will help you as much as it has helped me ;)
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">Collection</div>
-                    <div class="panel-body">
-                        <label for="collection">Collection-ID:</label>
-                        <input class="form-control" type="text" name="collection" id="form_collection"/>
-                        <br/>
-                        <button onclick="pull_collection()" class="btn btn-block btn-info">Get Mod-Ids</button>
+                <div class="col-md-9 col-sm-12">
+                    <div class="card text-white bg-dark">
+                        <div class="card-header">Input</div>
+                        <div class="card-body">
+                            <label for="collection">Collection-ID:</label>
+                            <input class="form-control" type="text" name="collection" id="form_collection"/>
+                            <br/>
+                            <button onclick="pull_collection()" class="btn btn-block btn-info">Get Mod-Ids</button>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="panel panel-info">
-                    <div class="panel-heading">Raw ( only IDs)</div>
-                    <div class="panel-body">
-                        <textarea id="raw_ids" rows="6" class="form-control"></textarea>
+                    <div class="row" style="margin-top: 15px;">
+                        <div class="col-md-6">
+                            <div class="card text-light bg-secondary">
+                                <div class="card-header">Raw ( only IDs)</div>
+                                <div class="card-body">
+                                    <textarea id="raw_ids" rows="6" class="form-control"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card text-light bg-secondary">
+                                <div class="card-header">Found <span class="badge badge-info" id="badge_count">0</span> Workshop Items</div>
+                                <table class="card-body table table-hover table-striped">
+                                    <thead>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Url</th>
+                                    </thead>
+                                    <tbody id="table_fillme">
+                                        <tr id="table_removeme"><td colspan="3"><span class="text-center">nothing here yet</span></td></tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="panel panel-warning">
-                    <div class="panel-heading">Found <span class="badge badge-info" id="badge_count"></span> Workshop Items</div>
-                    <table class="panel-body table table-hover table-striped">
-                        <thead>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Url</th>
-                        </thead>
-                        <tbody id="table_fillme">
-                            <tr id="table_removeme"><td colspan="3"><span class="text-center">nothing here yet</span></td></tr>
-                        </tbody>
-                    </table>
                 </div>
             </div>
         </div>
+        <?php include $templatedir."head/jsfiles.phtml"; ?>
+        <script>
+            function pull_collection() {
+                collectionid = $('#form_collection').val();
+                if (collectionid=="") {
+                    alert("Please fill out the ID Field in the form!");
+                    exit;
+                } else {
+                    dataString = "collection="+collectionid;
+                    $.ajax (
+                        {
+                            type: 'POST',
+                            url: 'https://tools.rusty.info/stcolids/ajax/getcol.php',
+                            data: dataString,
+                            cache: false,
+                            beforeSend: function() {
+                                //exec before sending
+                            },
+                            success: function(data) {
+                                //on message recieved
+                                var obj = JSON.parse(data);
+                                if (obj.type == 'success') {
+                                    //get steam stuff successfull
+                                    $('#table_removeme').remove();
+                                    $('#badge_count').append(obj.modscount)
+                                    var modsdata = obj.modsdata;
+                                    modsdata.forEach(function(entry) {
+                                        $('#raw_ids').append(entry.id+'\n');
+                                        $('#table_fillme').append('<tr><td>'+entry.id+'</td><td>'+entry.name+'</td><td><a href="'+entry.url+'">Workshop Link</a></td></tr>');
+                                    });
+                                } else {
+                                    //failed. display message
+                                    alert(obj.message);
+                                }
+                            }
+                        }
+                    )
+                }
+            }
+        </script>
     </body>
-    <footer>
-    </footer>
 </html>
