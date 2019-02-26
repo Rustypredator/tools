95ecf16d0581ca9177fcc72883b040b8868259b0
Florian <florian@steltenkamp.de>
2019-02-26 21:11:44 +0100

    reworked vigenere cipher. its broken now.

--- tools/vigenere/index.php
+++ tools/vigenere/index.php
@@ -21,12 +21,12 @@
  * @author Florian Steltenkamp <contact@rusty.info>
  */
 require "../../init.php";
-require "vigenere.php";
 $toolname = "Vigenere";
 $toolshort = strtolower($toolname);
 $toolDescShort = "";
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <?php include $templatedir."head/head.phtml"; ?>
     <body>
         <?php
@@ -42,63 +42,81 @@ $toolDescShort = "";
                     <div class="card text-white bg-success" style="margin-top:15px;">
                         <div class="card-header collapsed" type="button" data-toggle="collapse" data-target="#infoCollapse" aria-expanded="false" aria-controls="infoCollapse">INFO ( Click me! )</div>
                         <div class="card-body collapse" id="infoCollapse">
-                            {INFO CONTENT}
+                            <h1>Vigenére Cipher:</h1>
+                            <p>
+                                This tool enables you to encode/decode Vigenére Ciphers.<br/>
+                                You need to know the Key and the Alphabet that was used to encode the string to decode it.
+                            </p>
+                            <a href="https://en.wikipedia.org/wiki/Vigenère_cipher">more info (wikipedia)</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-9 col-sm-12">
+                    <?php include $templatedir."other/wipwarning.phtml"; ?>
                     <div class="card text-white bg-dark">
                         <div class="card-header">Input</div>
                         <div class="card-body">
-                            <form action="" method="post">
-                                <div class="form-group">
-                                    <label for="cipherAlphabet">Your Cipher Alphabet:</label>
-                                    <input type="text" id="cipherAlphabet" name="cipherAlphabet" class="form-control" disabled placeholder="ABCDEFGHIJKLMNOPQRSTUVWXYZ" value="<?php if(isset($_POST["cipherAlphabet"])) echo $_POST["cipherAlphabet"];?>">
-                                </div>
-                                <div class="form-group">
-                                    <label for="cipherKey">Your Cipher Key:</label>
-                                    <input type="text" id="cipherKey" name="cipherKey" class="form-control" placeholder="Your Cipher Key"  value="<?php if(isset($_POST["cipherKey"])) echo $_POST["cipherKey"];?>">
-                                </div>
-                                <div class="form-group">
-                                    <label for="cipherContent">Content (Text To En/De-crypt)</label>
-                                    <textarea id="cipherContent" name="cipherContent" class="form-control" rows="15"><?php if(isset($_POST["cipherContent"])) echo $_POST["cipherContent"]; else echo "Place your Text here...";?></textarea>
-                                </div>
-                                <input type="radio" value="enc" name="ende">Encrypt<br/>
-                                <input type="radio" value="dec" name="ende">Decrypt
-                                <input type="submit" class="btn btn-success btn-block" value="Make it So!">
-                            </form>
+                            <div class="form-group">
+                                <label for="cipherAlphabet">Your Cipher Alphabet:</label>
+                                <input type="text" id="cipherAlphabet" name="cipherAlphabet" class="form-control" disabled value="<?php if(isset($_POST["cipherAlphabet"])) echo $_POST["cipherAlphabet"]; else echo "ABCDEFGHIJKLMNOPQRSTUVWXYZ";?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="cipherKey">Your Cipher Key:</label>
+                                <input type="text" id="cipherKey" name="cipherKey" class="form-control" placeholder="Your Cipher Key"  value="<?php if(isset($_POST["cipherKey"])) echo $_POST["cipherKey"];?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="cipherContent">Content (Text To En/De-crypt)</label>
+                                <textarea id="cipherContent" name="cipherContent" class="form-control" rows="4"><?php if(isset($_POST["cipherContent"])) echo $_POST["cipherContent"]; else echo "Place your Text here...";?></textarea>
+                            </div>
+                            <div class="btn-group btn-block">
+                                <button onclick="process('en')" class="btn btn-success">Encrypt</button>
+                                <button onclick="process('de')" class="btn btn-primary">Decrypt</button>
+                            </div>
+                            <div class="form-group">
+                                <label for="cipherOutput">Output:</label>
+                                <textarea id="cipherOutput" name="cipherOutput" class="form-control" rows="4"></textarea>
+                            </div>
                         </div>
-                        <?php if(count($_POST) > 0) : ?>
-                        <div class="panel-body">
-                            <?php
-                                $key = strtolower($_POST["cipherKey"]);
-                                $content = strtolower($_POST["cipherContent"]);
-                                //$alphabetString = $_POST["cipherAlphabet"];
-                                $encDec = $_POST["ende"];
-                                $alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                                $alphabetTable = buildAlphabetTable($alphabetString);
-                                if ($encDec === "enc") {
-                                    $encrypted = encrypt($content, $key, $alphabetTable, $alphabetString);
-                                    echo "Your Encrypted Content:<br/>";
-                                    echo "<pre>";
-                                    echo $encrypted;
-                                    echo "</pre>";
-                                } elseif ($encDec === "dec") {
-                                    $decrypted = decrypt($content, $key, $alphabetTable, $alphabetString);
-                                    echo "Your Decrypted Content:<br/>";
-                                    echo "<pre style='color:white;'>";
-                                    echo $decrypted;
-                                    echo "</pre>";
-                                } else {
-                                    print_r("Please select an option!");
-                                }
-                            ?>
-                        </div>
-                        <?php endif; ?>
                     </div>
                 </div>
             </div>
         </div>
         <?php include $templatedir."head/jsfiles.phtml"; ?>
+        <script>
+            function process(type) {
+                if (type !== 'en' && type !== 'de') {
+                    toastr["error"]("Please use the Buttons to en/decrypt!");
+                    return;
+                }
+
+                let alphabet = $('#cipherAlphabet').val();
+                let cipherKey = $('#cipherKey').val();
+                let cipherString = $('#cipherContent').html();
+
+                let dataString = "cipherAlphabet=" + alphabet + "&cipherKey=" + cipherKey + "&cipherContent=" + cipherString + "&ende=" + type;
+
+                $.ajax
+                (
+                    {
+                        type: 'POST',
+                        url: toolurl + 'ajax/vigenere.php',
+                        data: dataString,
+                        cache: false,
+                        beforeSend: function() {},
+                        success: function(data) {
+                            let successobj = JSON.parse(data);
+                            let success = successobj.success;
+                            if(success) {
+                                toastr["success"](successobj.message);
+                                let output = successobj.data;
+                                $('#cipherOutput').html(output);
+                            } else {
+                                toastr["error"](successobj.message);
+                            }
+                        }
+                    }
+                )
+            }
+        </script>
     </body>
 </html>
