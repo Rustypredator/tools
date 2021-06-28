659f6053b92fb08648277685efde11b12d656e38
Rustypredator <contact@rusty.info>
2021-06-28 15:18:46 +0200

    refresh welcome page

--- resources/views/themes/default/pub/welcome.blade.php
+++ resources/views/themes/default/pub/welcome.blade.php
@@ -1,4 +1,22 @@
 @extends('themes.default.pub.master')
 
+@section('content')
+    <div class="row">
+        <div class="card bg-gradient-warning">
+            <div class="card-header border-0 ui-sortable-handle" style="cursor: move;">
+                <h3 class="card-title"><i class="fas fa-hands-helping mr-1"></i>Welcome</h3>
+                <!-- card tools -->
+                <div class="card-tools">
+                    <button type="button" class="btn btn-primary btn-sm" data-card-widget="collapse" title="Collapse"><i class="fas fa-minus"></i></button>
+                </div>
+                <!-- /.card-tools -->
+            </div>
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+@stop
+
 @section('client-js')
 @stop
