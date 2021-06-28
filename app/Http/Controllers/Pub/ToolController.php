eb35100df2d33e726e32213e16f4dbba02201338
Rustypredator <contact@rusty.info>
2021-06-28 21:15:01 +0200

    use correct request

--- app/Http/Controllers/Pub/ToolController.php
+++ app/Http/Controllers/Pub/ToolController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Pub;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class ToolController extends Controller
 {
