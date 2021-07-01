f5504201a74428a18ab70f6cb744058c7eb1b50f
Rustypredator <contact@rusty.info>
2021-07-01 18:35:47 +0200

    json stuff

--- resources/views/themes/default/pub/tools/pwgen.blade.php
+++ resources/views/themes/default/pub/tools/pwgen.blade.php
@@ -107,7 +107,7 @@
                     cache: false,
                     beforeSend: function() {},
                     success: function(data) {
-                        data = JSON.parse(data)
+                        //data = JSON.parse(data)
                         console.log(data)
                     }
                 }
