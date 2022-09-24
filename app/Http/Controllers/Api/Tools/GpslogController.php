82fa049c12ad8ebce696c97f610bb3845d860746
Rustypredator <contact@rusty.info>
2022-09-24 14:23:13 +0000

    add log facade

--- app/Http/Controllers/Api/Tools/GpslogController.php
+++ app/Http/Controllers/Api/Tools/GpslogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Tools;
 
 use App\Http\Controllers\Api\ToolsController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class GpslogController extends ToolsController
 {
